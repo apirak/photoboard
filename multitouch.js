f345a44e0c9af5c64e20be32b5d963095198534d
Apirak Panatkool <apirakb@gmail.com>
2010-07-06 11:24:58 +0700

    track element inside element

--- multitouch.js
+++ multitouch.js
@@ -1,14 +1,34 @@
-// Copyright (c) 2008-2010, Thomas Robinson (http://tlrobinson.net/)
-//
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-//
-// Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-// Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-// Neither the name of the Thomas Robinson nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
+/*
+ *	Copyright (c) 2008, Thomas L. Robinson
+ *	
+ *	All rights reserved.
+ *	
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions
+ *	are met:
+ *	
+ *	Redistributions of source code must retain the above copyright
+ *	notice, this list of conditions and the following disclaimer.
+ *	Redistributions in binary form must reproduce the above copyright
+ *	notice, this list of conditions and the following disclaimer in the
+ *	documentation and/or other materials provided with the distribution.
+ *	Neither the name of the tlrobinson.net nor the names of its contributors
+ *	may be used to endorse or promote products derived from this software
+ *	without specific prior written permission.
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *	A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ *	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 function hasClass(ele,cls) {
 	if (ele.className != null) {
 		return ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
@@ -22,22 +42,27 @@ function hasClass(ele,cls) {
 		function touchHandler(e) {
 			if (e.type == "touchstart") {
 				for (var i = 0; i < e.touches.length; i++) {
+					
+					var movableTarget = getMovableTarget(e.touches[i].target);
 				    // for each "movable" touch event:
   				//alert("x:"+e.touches[i].target)
-					console.log("touch start " + i + " " + e.touches[i].target.className + " id:" + e.touches[i].target.id);
-					console.log("movable element: " + movableTarget(e.touches[i].target).className);
-					if (hasClass(e.touches[i].target, "movable")) {
+					//console.log("touch classname: " + e.touches[i].target.className + " id:" + e.touches[i].target.id);
+					//console.log("movable classname: " + movableTarget.className);
+					//console.log("touch identifier: " + e.touches[i].identifier);
+					//console.log("movable value:" + movableTarget.xfmTX);
+					
+					if (hasClass(movableTarget, "movable")) {
 						
 						var id = e.touches[i].identifier;
 						
 						// record initial data in the "moving" hash
 						moving[id] = {
 							identifier: id,
-							target:   	e.touches[i].target,
+							target:   	movableTarget,
 							mouse:		{ x: e.touches[i].clientX, y: e.touches[i].clientY },
-							position:	{ x: e.touches[i].target.xfmTX, y: e.touches[i].target.xfmTY },
-							rotation: 	e.touches[i].target.xfmR,
-							scale: 		e.touches[i].target.xfmS
+							position:	{ x: movableTarget.xfmTX, y: movableTarget.xfmTY },
+							rotation: 	movableTarget.xfmR,
+							scale: 		movableTarget.xfmS
 						};
 						
 						// move to the front
@@ -48,12 +73,13 @@ function hasClass(ele,cls) {
 					}
 				}
 			}
-			else if (e.type == "touchmove") {
+			else if (e.type == "touchmove") {			
 			    // if there are two touchs and both are on the *same* element, we're in rotate/scale mode
-				if (e.touches.length == 2 && e.touches[0].target == e.touches[1].target) {
+				if (e.touches.length == 2 && 
+					getMovableTarget(e.touches[0].target) == getMovableTarget(e.touches[1].target)) {
 					var idA = e.touches[0].identifier,
 						idB = e.touches[1].identifier;
-					
+										
 					// if we've previously recorded initial rotate/scale mode data:
 					if (moving[idA] != null && moving[idA].rotateScaleMode && moving[idB].rotateScaleMode ) {
 					    // calculate translation, rotation, and scale
@@ -113,7 +139,7 @@ function hasClass(ele,cls) {
 				'rotate('+element.xfmR+'deg)';
 		}
 		
-		function movableTarget(element){
+		function getMovableTarget(element){
 			top_element = element;
 			while (top_element.id == "") {
 				top_element = top_element.parentNode;
